0a8c7dec961419b68673ffb9c469edc572b00b2a
unknown <2916963017@qq.com>
2025-10-04 23:00:05 +0800

    bugfix

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 class GodMan :
     def __init__(self):
-        luck = 10
-        health = 10
-        money = 1000
-        iq = 10
-        family = 0
+        self.luck = 10
+        self.health = 10
+        self.money = 1000
+        self.iq = 10
+        self.family = 0
         pass
